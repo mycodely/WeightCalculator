24be5ce09ce1be19452b9a1538e9b6ea98ea8368
Mycodely <ekyua96@gmail.com>
2023-04-10 14:01:18 -0300

    Reworked sections, added table with calculated results, need to improve css

--- js/calculadoraPeso.js
+++ js/calculadoraPeso.js
@@ -15,8 +15,52 @@ const handleCalculateClick = () => {
   weightCalculator.generateWeeklyReg(weightCalculator.weeklyCoefficient);
 
   // Mostrar los resultados en la página
-  document.getElementById("estimated-time").textContent = `El tiempo estimado en semanas para llegar al peso objetivo es: ${weightCalculator.estimatedTime}`;
-  document.getElementById("weekly-reg").textContent = `Tu registro semanal de peso es: ${weightCalculator.weeklyReg}`;
+
+  const estimatedTime = document.getElementById('estimated-time');
+document.documentElement.style.setProperty('--estimated-time-height', estimatedTime.offsetHeight + 'px');
+
+document.getElementById("estimated-time").textContent = `The estimated time in weeks to reach the target weight is: ${weightCalculator.estimatedTime}`;
+// Obtenemos la referencia al elemento donde queremos mostrar la tabla
+const weeklyRegElement = document.getElementById("weekly-reg");
+
+// Creamos la tabla y sus elementos
+const table = document.createElement("table");
+const thead = document.createElement("thead");
+const tbody = document.createElement("tbody");
+
+// Agregamos los encabezados de las columnas
+const headers = ["Week", "Weight"];
+const headerRow = document.createElement("tr");
+headers.forEach(headerText => {
+  const header = document.createElement("th");
+  header.textContent = headerText;
+  headerRow.appendChild(header);
+});
+thead.appendChild(headerRow);
+
+// Agregamos los datos de cada semana
+const weeklyReg = weightCalculator.weeklyReg;
+
+for (let i = 0; i < weeklyReg.length; i++) {
+  const row = document.createElement("tr");
+  const weekCell = document.createElement("td");
+  weekCell.textContent = `${i + 1}`;
+  const weightCell = document.createElement("td");
+  weightCell.textContent = weeklyReg[i];
+  row.appendChild(weekCell);
+  row.appendChild(weightCell);
+  tbody.appendChild(row);
+}
+
+// Agregamos los elementos a la tabla
+
+table.appendChild(thead);
+table.appendChild(tbody);
+
+// Agregamos la tabla al elemento en el HTML
+weeklyRegElement.appendChild(table);
+
+
 };
 
 // Función para manejar el evento de clic en el botón "Resetear"
